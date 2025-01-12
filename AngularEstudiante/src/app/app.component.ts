28192ed720e8121e648625166d55a39b8897109f
j.sanchez <jodsuedrk@gmail.com>
2025-01-11 22:41:29 -0300

    Se agrego ngModel, se agrego guardado de variable local host del DataBinding

--- AngularEstudiante/src/app/app.component.ts
+++ AngularEstudiante/src/app/app.component.ts
@@ -1,16 +1,25 @@
 import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+/*@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})*/
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
   @ViewChild('myModal') model : ElementRef | undefined;
+  studentObj: Student = new Student();
 
   title = 'AngularEstudiante';
 
@@ -32,4 +41,47 @@ export class AppComponent {
 
   }
 
+  saveStudent(){
+    debugger;
+    
+    const isLocalPresent = localStorage.getItem("angular17crud");
+
+    if(isLocalPresent != null){
+
+      const oldArray = JSON.parse(isLocalPresent);
+      oldArray.push(this.studentObj);
+      localStorage.setItem('angular17crud', JSON.stringify(oldArray));
+
+    }else{
+
+      const newArr = [];
+      newArr.push(this.studentObj);
+      localStorage.setItem('angular17crud', JSON.stringify(newArr));
+
+    }
+
+  }
+
+}
+
+
+//  Creatin Classes
+export class Student{
+  name: string;
+  mobileNo: string;
+  email: string;
+  city: string;
+  state: string;
+  pincode: string;
+  address: string;
+
+  constructor(){
+    this.address = '';
+    this.city = '';
+    this.email = '';
+    this.mobileNo = '';
+    this.name = '';
+    this.state = '';
+    this.pincode = '';
+  }
 }
