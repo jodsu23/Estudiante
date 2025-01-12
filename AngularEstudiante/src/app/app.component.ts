64d35131516854123d7b56a4139469eb12dc423a
j.sanchez <jodsuedrk@gmail.com>
2025-01-11 23:45:07 -0300

    Agregamos el clear and btn card en index

--- AngularEstudiante/src/app/app.component.ts
+++ AngularEstudiante/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 /*@Component({
   selector: 'app-root',
@@ -16,15 +17,26 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit {
   @ViewChild('myModal') model : ElementRef | undefined;
   studentObj: Student = new Student();
+  studentList: Student[] = [];
+
+  ngOnInit(): void {
+    const localData = localStorage.getItem("angular17crud");
+
+    if(localData != null){
+      this.studentList = JSON.parse(localData);
+    }
+
+  }
 
   title = 'AngularEstudiante';
 
   openModel(){
-
+    // Esta linea setea nuevamente el modelo cuando se apertura el model, asi no quedan datos cacheados anteriormente.
+    this.studentObj = new Student();
+    
     const model = document.getElementById("myModal");
 
     if(model != null){
@@ -50,15 +62,20 @@ export class AppComponent {
 
       const oldArray = JSON.parse(isLocalPresent);
       oldArray.push(this.studentObj);
+      // enviamos data al arraycard principal, actualizado.
+      this.studentList = oldArray;
       localStorage.setItem('angular17crud', JSON.stringify(oldArray));
 
     }else{
 
       const newArr = [];
       newArr.push(this.studentObj);
+      this.studentList = newArr;
       localStorage.setItem('angular17crud', JSON.stringify(newArr));
 
     }
+
+    this.closeModel();
 
   }
 
